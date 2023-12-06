2c699f3074f90be040d263551828cf5526346312
Nathanael <nathanael.weber@students.fhnw.ch>
2023-12-06 23:15:02 +0100

    feat: solution for day 1 part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,52 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let result = input.split('\n')
+        .filter(|line| {!line.is_empty()})
+        .map(|line| {
+            let chars = line.chars();
+            let first_digit = chars.clone().find(|c| { c.is_ascii_digit() })
+                .expect(&*format!("no digit found: {line}"))
+                .to_digit(10)
+                .expect("digit was no digit") * 10;
+            let last_digit = chars.clone().rfind(|c| { c.is_ascii_digit() })
+                .expect(&*format!("no digit found: {line}"))
+                .to_digit(10)
+                .expect("digit was no digit");
+
+            first_digit + last_digit
+        })
+        .sum();
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let result = input.split('\n')
+        .filter(|line| {!line.is_empty()})
+        .map(|line| {
+
+
+            12
+        })
+        .sum();
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
