b5c58f27d7e634cd73cb5d6333b5c8cdd5a9ee5d
Nathanael <nathanael.weber@students.fhnw.ch>
2023-12-07 16:59:26 +0100

    feat: solve day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -21,38 +21,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 
     let result = input.lines()
         .filter(|line| !line.is_empty())
-        .map(|line| {
-            let [game, plays] = (line.split(':').collect::<Vec<&str>>())[0..2] else {
-                panic!("split did not work, invalid format on line {line}")
-            };
-
-            let id = game.strip_prefix("Game ")
-                .unwrap_or("0")
-                .parse::<u32>()
-                .unwrap_or(0);
-
-            let parsed_plays = plays.split(';')
-                .map(|play| {
-                    play.split(',')
-                        .map(|color_play| {
-                            let [amount, color] = (color_play.trim().split(' ').collect::<Vec<&str>>())[0..2] else {
-                                panic!("color play did not match see {color_play}");
-                            };
-
-                            let converted_amount = amount.parse::<u32>().unwrap_or(0);
-                            match color {
-                                "blue" => Play { color: Color::BLUE, amount: converted_amount },
-                                "red" => Play { color: Color::RED, amount: converted_amount },
-                                "green" => Play { color: Color::GREEN, amount: converted_amount },
-                                _ => panic!("color play did not match color see {color_play}")
-                            }
-                        })
-                        .collect::<Vec<Play>>()
-                })
-                .collect::<Vec<Vec<Play>>>();
-
-            Game { id: id, plays: parsed_plays }
-        })
+        .map(parse_game)
         .filter(|game| {
             !game.plays.iter().any(|play| {
                 play.iter().any(|color_play| {
@@ -76,8 +45,65 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result)
 }
 
+fn parse_game(line: &str) -> Game {
+    let [game, plays] = (line.split(':').collect::<Vec<&str>>())[0..2] else {
+        panic!("split did not work, invalid format on line {line}")
+    };
+
+    let id = game.strip_prefix("Game ")
+        .unwrap_or("0")
+        .parse::<u32>()
+        .unwrap_or(0);
+
+    let parsed_plays = plays.split(';')
+        .map(|play| {
+            play.split(',')
+                .map(|color_play| {
+                    let [amount, color] = (color_play.trim().split(' ').collect::<Vec<&str>>())[0..2] else {
+                        panic!("color play did not match see {color_play}");
+                    };
+
+                    let converted_amount = amount.parse::<u32>().unwrap_or(0);
+                    match color {
+                        "blue" => Play { color: Color::BLUE, amount: converted_amount },
+                        "red" => Play { color: Color::RED, amount: converted_amount },
+                        "green" => Play { color: Color::GREEN, amount: converted_amount },
+                        _ => panic!("color play did not match color see {color_play}")
+                    }
+                })
+                .collect::<Vec<Play>>()
+        })
+        .collect::<Vec<Vec<Play>>>();
+
+    Game { id: id, plays: parsed_plays }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let result = input.lines()
+        .filter(|line| !line.is_empty())
+        .map(parse_game)
+        .map(|game| {
+            let (mut min_red, mut min_blue, mut min_green) = (0, 0, 0);
+
+            for play in game.plays.iter().flatten() {
+                match play.color {
+                    Color::GREEN => {
+                        if play.amount > min_green { min_green = play.amount }
+                    }
+                    Color::RED => {
+                        if play.amount > min_red { min_red = play.amount }
+                    }
+                    Color::BLUE => {
+                        if play.amount > min_blue { min_blue = play.amount }
+                    }
+                }
+            }
+
+            min_red * min_blue * min_green
+        })
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -92,7 +118,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(2286));
     }
 }
