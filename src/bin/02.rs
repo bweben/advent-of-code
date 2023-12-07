f4f5b0fe70d33fc9ba7db5f443402762f9c17f8b
Nathanael <nathanael.weber@students.fhnw.ch>
2023-12-07 17:01:37 +0100

    refactor: extract methods

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -58,24 +58,26 @@ fn parse_game(line: &str) -> Game {
     let parsed_plays = plays.split(';')
         .map(|play| {
             play.split(',')
-                .map(|color_play| {
-                    let [amount, color] = (color_play.trim().split(' ').collect::<Vec<&str>>())[0..2] else {
-                        panic!("color play did not match see {color_play}");
-                    };
-
-                    let converted_amount = amount.parse::<u32>().unwrap_or(0);
-                    match color {
-                        "blue" => Play { color: Color::BLUE, amount: converted_amount },
-                        "red" => Play { color: Color::RED, amount: converted_amount },
-                        "green" => Play { color: Color::GREEN, amount: converted_amount },
-                        _ => panic!("color play did not match color see {color_play}")
-                    }
-                })
+                .map(parse_play)
                 .collect::<Vec<Play>>()
         })
         .collect::<Vec<Vec<Play>>>();
 
     Game { id: id, plays: parsed_plays }
+}
+
+fn parse_play(color_play: &str) -> Play {
+    let [amount, color] = (color_play.trim().split(' ').collect::<Vec<&str>>())[0..2] else {
+        panic!("color play did not match see {color_play}");
+    };
+
+    let converted_amount = amount.parse::<u32>().unwrap_or(0);
+    match color {
+        "blue" => Play { color: Color::BLUE, amount: converted_amount },
+        "red" => Play { color: Color::RED, amount: converted_amount },
+        "green" => Play { color: Color::GREEN, amount: converted_amount },
+        _ => panic!("color play did not match color see {color_play}")
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
