12810b1b8253cb54be2435758fb9661ada891d89
Nathanael <nathanael.weber@students.fhnw.ch>
2023-12-08 15:40:49 +0100

    feat: solve day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -3,21 +3,36 @@ advent_of_code::solution!(4);
 struct Card {
     id: u32,
     winning_numbers: Vec<u32>,
-    drawn_numbers: Vec<u32>
+    drawn_numbers: Vec<u32>,
 }
 
 impl Card {
     fn get_points(&self) -> u32 {
+        let power = self.get_amount_winning();
+        if power == 0 {
+            return 0;
+        }
+
+        2u32.pow(power - 1)
+    }
+
+    fn get_winning_cards(&self) -> Vec<u32> {
+        let amount = self.get_amount_winning();
+        let mut result: Vec<u32> = Vec::new();
+
+        for i in 0..amount {
+            result.push(self.id + i + 1);
+        }
+
+        result
+    }
+
+    fn get_amount_winning(&self) -> u32 {
         let numbers_winning = self.drawn_numbers.iter()
             .filter(|drawn| self.winning_numbers.contains(drawn))
             .collect::<Vec<&u32>>();
 
-        let power = numbers_winning.len() as u32;
-        if power == 0 {
-            return 0
-        }
-
-        2u32.pow(power - 1)
+        numbers_winning.len() as u32
     }
 }
 
@@ -26,8 +41,9 @@ fn parse_card(line: &str) -> Card {
         panic!("split did not work, invalid format on line {line}")
     };
 
-    let id = card.strip_prefix("Card ")
-        .unwrap_or("0")
+    let id = card.strip_prefix("Card")
+        .unwrap()
+        .trim()
         .parse::<u32>()
         .unwrap_or(0);
 
@@ -59,7 +75,20 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let cards: Vec<Card> = input.lines()
+        .filter(|line| !line.is_empty())
+        .map(parse_card)
+        .collect();
+    let mut count = vec![1; cards.len()];
+
+    for card in cards {
+        for winning_card in card.get_winning_cards() {
+            let card_count = (count[card.id as usize - 1] * 1) + count[winning_card as usize - 1];
+            count[winning_card as usize - 1] = card_count;
+        }
+    }
+
+    Some(count.iter().sum())
 }
 
 #[cfg(test)]
@@ -75,6 +104,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
